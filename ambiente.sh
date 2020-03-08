09ea6eac502bf31d4ab2989ef95c6736fcc74545
iancesarvidinharego <iancesarvidinharego>
2020-03-08 10:55:08 -0300

    Update ambiente.sh

--- ambiente.sh
+++ ambiente.sh
@@ -76,36 +76,6 @@ echo "Instalando DBeaver"
 wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O DBeaver.deb
 apt install ./DBeaver.deb -y
 
-echo "Instalando Docker e Docker Compose"
-
-# https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo apt-get install -y \
- apt-transport-https \
- ca-certificates \
- curl \
- gnupg-agent \
- software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-echo -e "\ndeb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee -a /etc/apt/sources.list
-
-sudo apt-get update
-sudo apt-get install docker.io -y
-sudo apt-get autoremove -y
-
-# https://docs.docker.com/compose/install/
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
-
-# https://docs.docker.com/compose/completion/
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-
-# https://docs.docker.com/install/linux/linux-postinstall/
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
 echo "Instalando o STS"
 wget https://download.springsource.com/release/STS4/4.5.1.RELEASE/dist/e4.14/spring-tool-suite-4-4.5.1.RELEASE-e4.14.0-linux.gtk.x86_64.tar.gz -O STS.tar.gz
 tar -xvzf STS.tar.gz -C /home/ian/Dev/Java
